36edbd6045a2d993177c5972476ed38f919074ea
valadaa48 <valadaa48@gmx.com>
2021-01-17 22:14:54 -0500

    OGS

--- srcpkgs/rr-base/files/50-oga-resume.sh
+++ srcpkgs/rr-base/files/50-oga-resume.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 alsactl restore -f /var/asound.state
-if [ $(grep v11 /sys/devices/platform/odroidgo2-joypad/input/input2/name) ]; then
-    modprobe -i esp8089
-fi
+modprobe -i esp8089 || true
 modprobe -i dwc2
 sv start ogage
--- srcpkgs/rr-base/files/50-oga-suspend.sh
+++ srcpkgs/rr-base/files/50-oga-suspend.sh
@@ -3,6 +3,4 @@
 alsactl store -f /var/asound.state
 sv stop ogage
 modprobe -r dwc2
-if [ $(grep v11 /sys/devices/platform/odroidgo2-joypad/input/input2/name) ]; then
-    modprobe -r esp8089
-fi
+modprobe -r esp8089 || true
