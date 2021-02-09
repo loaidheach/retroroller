c136c42b8fbacf9a9a2d5ceb7355c890621d8faa
valadaa48 <valadaa48@gmx.com>
2021-02-08 23:37:27 -0500

    Updates

--- srcpkgs/rr-base/files/firstboot.sh
+++ srcpkgs/rr-base/files/firstboot.sh
@@ -6,10 +6,22 @@ if [ ! -f /firstboot ]; then
     exit 0
 fi
 
+echo "  ______ ______ ___ ___ _____   " >> /dev/tty1
+echo " |   __ \   __ \   |   |     |_ " >> /dev/tty1
+echo " |      <      <   |   |       |" >> /dev/tty1
+echo " |___|__|___|__|\_____/|_______|" >> /dev/tty1
+echo >> /dev/tty1
+echo >> /dev/tty1
+echo >> /dev/tty1
+echo "* Initializing ALSA" >> /dev/tty1
+
 amixer set 'Playback Path' 'SPK'
 alsactl store
+
+echo "* Setting background light to sane level" >> /dev/tty1
 light -S 50
 
+echo "* Resizing root partition" >> /dev/tty1
 sync
 
 rootpart=/dev/mmcblk0p2
@@ -21,17 +33,22 @@ EOF
 
 sleep 5
 
+echo "* Waiting for root to settle" >> /dev/tty1
 udevadm settle
 
 sleep 5
 
+echo "* Actually resize root partition" >> /dev/tty1
 partprobe $rootdev
 
 mount -o remount,rw $rootpart
 resize2fs $rootpart &> /dev/null
 
+echo "* Syncing disks" >> /dev/tty1
 sync
 
 touch /forcefsck
 rm /firstboot
 
+clear > /dev/tty1
+printf "\033c" > /dev/tty1
