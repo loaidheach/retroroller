e85ae2a1f38d779001151d871444711eecb57665
valadaa48 <valadaa48@gmx.com>
2021-02-13 01:27:52 -0500

    Revert dwc2 module temporarily

--- srcpkgs/rr-base/files/50-oga-suspend.sh
+++ srcpkgs/rr-base/files/50-oga-suspend.sh
@@ -3,8 +3,8 @@
 #alsactl store
 sv stop ogage
 
-#modprobe -r dwc2
-#lsmod | tail -n+2 | cut -d' ' -f1 > /tmp/zzz_modules
-#for x in $(cat /tmp/zzz_modules); do
-#    modprobe -r $x || true
-#done
+modprobe -r dwc2
+lsmod | tail -n+2 | cut -d' ' -f1 > /tmp/zzz_modules
+for x in $(cat /tmp/zzz_modules); do
+    modprobe -r $x || true
+done
