75056c0d8bb75eef4af1c9259bf2970e8c351392
loaidheach <loaidheach@chaoticneutral.eu>
2021-04-29 20:09:40 +0200

    added missing compiler flags

--- common/cross-profiles/aarch64.sh
+++ common/cross-profiles/aarch64.sh
@@ -7,7 +7,7 @@ XBPS_CFLAGS="-O2 -pipe" # XXX not yet supported: -fstack-protector-strong
 XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CFLAGS="-march=armv8-a+crc+fp+simd -mtune=cortex-a35 -mcpu=cortex-a35 -pipe -O2 -U_FORTIFY_SOURCE -fno-stack-protector -fno-stack-clash-protection -fomit-frame-pointer"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
-XBPS_CROSS_FFLAGS=""
+XBPS_CROSS_FFLAGS="$XBPS_CROSS_CFLAGS"
 XBPS_CROSS_RUSTFLAGS="--sysroot=${XBPS_CROSS_BASE}/usr"
 XBPS_CROSS_RUST_TARGET="aarch64-unknown-linux-gnu"
 
